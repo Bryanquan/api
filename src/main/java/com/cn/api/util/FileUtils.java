8a04c14a9831b52ecf80f28361d751f75c99ecbd
qba <jinboan@egova.com.cn>
2019-07-31 08:54:47 +0800

    [M]增加NIO

--- src/main/java/com/cn/api/util/FileUtils.java
+++ src/main/java/com/cn/api/util/FileUtils.java
@@ -1,9 +1,18 @@
 package com.cn.api.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileUtils {
+      public static final Logger logger = LoggerFactory.getLogger(FileUtils.class);
 
       public static final List<String> FILE_TYPE = new ArrayList<>();
 
@@ -43,5 +52,37 @@ public class FileUtils {
              return types.contains(suffix);
       }
 
+    public static void main(String[] args) {
+              readFile("E:/LexTeng/Desktop/小诸葛.txt");
+    }
+      public static void readFile(String path){
+          FileInputStream fileInputStream = null;
+          FileChannel fileChannel = null;
+          try {
+              fileInputStream = new FileInputStream(path);//打开文件
+              fileChannel = fileInputStream.getChannel();//文件通道
+              ByteBuffer byteBuffer = ByteBuffer.allocate(1024);//分配缓冲空间
+              fileChannel.read(byteBuffer);//将文件读取缓冲内存空间
+          }catch (FileNotFoundException e){
+                  logger.error(e.getMessage(),e);
+          }catch (IOException e){
+                  logger.error(e.getMessage(),e);
+          }finally {
+              if (CommonUtils.isNotNull(fileInputStream)){
+                  try {
+                      fileInputStream.close();
+                  }catch (IOException e){
+                      logger.error(e.getMessage(),e);
+                  }
+              }
+              if (CommonUtils.isNotNull(fileChannel)){
+                  try {
+                      fileChannel.close();
+                  }catch (Exception e){
+                      logger.error(e.getMessage(),e);
+                  }
 
+              }
+          }
+      }
 }
